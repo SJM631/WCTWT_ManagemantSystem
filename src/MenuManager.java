3d5f281fd9036124dd061a80bdac8d7ca1b384fa
SJM631 <konggogizoa@gmail.com>
2021-03-15 16:31:56 +0900

    removed hello.java and added MenuManger.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Clothes");
+		System.out.println("2. Delete Clothes");
+		System.out.println("3. Edit Clothes");
+		System.out.println("4. View Clothes");
+		System.out.println("5. Exit");
+	}
+
+}
